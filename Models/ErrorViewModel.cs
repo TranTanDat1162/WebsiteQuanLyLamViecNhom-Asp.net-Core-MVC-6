b38d0191365d3d2861a1ab59808553c267b010de
TranTanDat1162 <dattt11062002@gmail.com>
2023-11-16 22:09:29 +0700

    First Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebsiteQuanLyLamViecNhom.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
