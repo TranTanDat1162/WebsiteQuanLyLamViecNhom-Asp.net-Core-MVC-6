bb1d39574ad89262cee577d44f2149a8de8b2127
TranTanDat1162 <dattt11062002@gmail.com>
2023-11-17 16:17:22 +0700

    Create layout for Admin with 3 main tabs (not detailed in each tabs)

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteQuanLyLamViecNhom.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "UEF - Quản lý làm việc nhóm";
+            return View();
+        }
+    }
+}
