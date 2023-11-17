036be5ae7a90a3f72a976709aed211d1215dc91c
TranTanDat1162 <dattt11062002@gmail.com>
2023-11-17 16:35:39 +0700

    Create layout for Admin with 3 main tabs (not detailed in each tabs) (Updated)

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Admin}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
