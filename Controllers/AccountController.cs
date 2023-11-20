821a731736f2643cefa0fdb11e5d0f5f8354e014
TranTanDat1162 <dattt11062002@gmail.com>
2023-11-20 13:30:14 +0700

    Create Login Layout with additional functions (Forgot password, Confirm Email...)

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteQuanLyLamViecNhom.Controllers
+{
+    public class AccountController : Controller
+    {
+        // Giao diện đăng nhập
+        public IActionResult Login()
+        {
+            ViewData["Title"] = "UEF - Quản lý làm việc nhóm";
+            return View();
+        }
+        // Giao diện nhập email để lấy mật khẩu
+        public IActionResult ForgotPassword()
+        {
+            ViewData["Title"] = "UEF - Quản lý làm việc nhóm";
+            return View();
+        }
+        // Giao diện khi mail gửi thành công
+        public IActionResult MailConfirm()
+        {
+            ViewData["Title"] = "UEF - Quản lý làm việc nhóm";
+            return View();
+        }
+        // Giao diện đổi lại mật khẩu mới
+        public IActionResult ResetPassword()
+        {
+            ViewData["Title"] = "UEF - Quản lý làm việc nhóm";
+            return View();
+        }
+        // Giao diện đổi mật khẩu thành công
+        public IActionResult PasswordConfirm()
+        {
+            ViewData["Title"] = "UEF - Quản lý làm việc nhóm";
+            return View();
+        }
+    }
+}
