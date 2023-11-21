f70c2b66c15b2a5dddc9056ff1e14fc2ad165d94
LmTrieu <lmtrieu250902@gmail.com>
2023-11-21 15:17:29 +0700

    Revert "Revert "Merge branch 'BranchMinhTrieu'""
    This reverts commit ced5687ef8143ef63f981848c9fb64bb722f1c87.

---
+++ Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteQuanLyLamViecNhom.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Password{ get; set; }
+    }
+}
