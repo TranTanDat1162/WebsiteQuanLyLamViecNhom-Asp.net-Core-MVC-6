3d887d9b0933b1f5dfe669470cfb10d3cbdcfb0b
TranTanDat1162 <dattt11062002@gmail.com>
2023-12-04 11:44:31 +0700

    Change TeacherCode in right order

---
+++ Data/Migrations/20231204034942_merge_data_2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebsiteQuanLyLamViecNhom.Data.Migrations
+{
+    public partial class merge_data_2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
