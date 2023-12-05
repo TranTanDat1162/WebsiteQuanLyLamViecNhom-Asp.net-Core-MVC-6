b2aabee08630e50de48458ea5a8076f8f4f40a5f
TranTanDat1162 <dattt11062002@gmail.com>
2023-12-05 14:30:28 +0700

    Update _TeacherLayout

--- Controllers/TeacherController.cs
+++ Controllers/TeacherController.cs
@@ -1,12 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebsiteQuanLyLamViecNhom.Data;
+using WebsiteQuanLyLamViecNhom.Models;
 
 namespace WebsiteQuanLyLamViecNhom.Controllers
 {
     public class TeacherController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public TeacherController(ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var teacher = await _context.Teacher.FirstOrDefaultAsync(); 
+            string teacherCode = teacher?.TeacherCode;
+            string teacherImgId = teacher?.ImgId;
+
+            var viewModel = new Teacher
+            {
+                TeacherCode = teacherCode,
+                ImgId = teacherImgId
+            };
+
+            return View(viewModel);
         }
         public IActionResult TeacherClass()
         {
