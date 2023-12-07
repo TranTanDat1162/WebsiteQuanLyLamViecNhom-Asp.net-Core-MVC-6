2330504dd575b2111e5e24631de54fd7263c3b50
LmTrieu <lmtrieu250902@gmail.com>
2023-12-07 11:56:25 +0700

    Recover StudentController
    Read title

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteQuanLyLamViecNhom.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProjectDetail()
+        {
+            return View();
+        }
+    }
+}
