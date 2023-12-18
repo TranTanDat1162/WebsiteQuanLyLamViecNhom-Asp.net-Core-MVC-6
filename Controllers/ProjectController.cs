f70511448cc145037c94ed7345570e59267717fe
TranTanDat1162 <dattt11062002@gmail.com>
2023-12-19 02:28:36 +0700

    Config Project Layout
    - Add TeacherProjectLayout and StudentProjectLayout for manifest their code and image (for teacher), also dynamically use layout for approriate role.

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -1,20 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebsiteQuanLyLamViecNhom.Data;
+using WebsiteQuanLyLamViecNhom.Models;
 
 namespace WebsiteQuanLyLamViecNhom.Controllers
 {
     public class ProjectController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Models.BaseApplicationUser> _userManager;
+
+        public ProjectController(ApplicationDbContext context, UserManager<BaseApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        static Student? viewModelStudent = new Student
+        {
+            StudentCode = "Student"
+        };
+
+        static Teacher? viewModelTeacher = new Teacher
+        {
+            TeacherCode = "Teacher"
+        };
+
         //TO-DO:
         //https://stackoverflow.com/questions/37554536/ho-do-i-show-a-button-that-links-to-a-page-only-if-the-user-is-authorized-to-vie
         // Return View for Teacher
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            viewModelTeacher = await _context.Teacher.FindAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            ViewData["Teacher"] = viewModelTeacher;
             return View();
         }
 
         // Return View for Student
-        public IActionResult Student()
+        public async Task<IActionResult> Student()
         {
+            viewModelStudent = await _context.Student.FindAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            ViewData["Student"] = viewModelStudent;
             return View();
         }
     }
