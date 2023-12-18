54cc1e5d344b75d537018bf4db79c6936df3710d
TranTanDat1162 <dattt11062002@gmail.com>
2023-12-19 01:23:33 +0700

    Fix Layout
    - Add student code to student Layout
    - Add view Student to Project Controller Base On ProjectDetail in controller Student

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -6,10 +6,16 @@ namespace WebsiteQuanLyLamViecNhom.Controllers
     {
         //TO-DO:
         //https://stackoverflow.com/questions/37554536/ho-do-i-show-a-button-that-links-to-a-page-only-if-the-user-is-authorized-to-vie
+        // Return View for Teacher
         public IActionResult Index()
         {
             return View();
         }
 
+        // Return View for Student
+        public IActionResult Student()
+        {
+            return View();
+        }
     }
 }
--- Controllers/StudentController.cs
+++ Controllers/StudentController.cs
@@ -1,12 +1,42 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using WebsiteQuanLyLamViecNhom.Data;
+using WebsiteQuanLyLamViecNhom.Models;
 
 namespace WebsiteQuanLyLamViecNhom.Controllers
 {
     public class StudentController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<Models.BaseApplicationUser> _userManager;
+        private readonly ILogger<StudentController> _logger;
+
+        static Student? viewModel = new Student
         {
-            return View();
+            StudentCode = "Student"
+        };
+
+        public StudentController(ApplicationDbContext context, UserManager<Models.BaseApplicationUser> userManager, ILogger<StudentController> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            // Lấy thông tin người dùng đăng nhập
+            viewModel = await _context.Student.FindAsync(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            // Kiểm tra xem người dùng có tồn tại không
+            if (viewModel != null)
+            {
+                ViewData["Student"] = viewModel;
+                return View();
+            }
+
+            // Xử lý trường hợp không có người dùng đăng nhập
+            return NotFound();
         }
         public IActionResult ProjectDetail()
         {
