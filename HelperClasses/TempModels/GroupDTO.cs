cd55649252264595cc216568f6ecf10a97203364
LmTrieu <lmtrieu250902@gmail.com>
2023-12-27 14:09:38 +0700

    DowloadButtonForStudent
    and Initials for GradeGroup

--- HelperClasses/TempModels/GroupDTO.cs
+++ HelperClasses/TempModels/GroupDTO.cs
@@ -9,6 +9,7 @@ namespace WebsiteQuanLyLamViecNhom.HelperClasses.TempModels
             public string MOTD { get; set; }
             public int ProjectId { get; set; }
             public string ProjectName { get; set; }
+            public string? ProjectAttachmentsJSON { get; set; }
             public string LeaderID { get; set; }
             public string LeaderName { get; set; } = string.Empty;
             public Class CurrentClass { get; set; }
@@ -28,6 +29,14 @@ namespace WebsiteQuanLyLamViecNhom.HelperClasses.TempModels
             public string Description { get; set; }
             public float? Grade { get; set; }
         }
+        public class GradeTaskDTO
+        {
+            public string TaskID { get; set; }
+            public string Description { get; set; }
+            public float? Grade { get; set; }
+        }
+        public GradeTaskDTO gradeTaskDTO { get; set; }
+
         public UpdateTaskDTO updateTaskDTO { get; set; }
         public TaskDTO createTaskDTO { get; set; }
         public GroupVM GroupViewModel {  get; set; }
