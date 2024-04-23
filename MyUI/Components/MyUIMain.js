1dc05bfb45b9d97aca09cbd3d7da02ed2828e206
LmTrieu <lmtrieu250902@gmail.com>
2024-04-23 16:30:19 +0700

    gave up

---
+++ MyUI/Components/MyUIMain.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export const MotoUIMain = () => {
+    return (
+        <div>Hello from React</div>
+    )
+}
+
+ReactDOM.render(
+    <MyUIMain />,
+    document.getElementById('myuimain'),
+);
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: 'development',
+
+    devtool: 'inline-source-map',
+
+    entry: ['babel-polyfill', './MyUI/Components/MyUIMain.js'],
+
+    output: {
+        filename: 'myui-bundle.js',
+        path: path.join(__dirname, 'wwwroot/dist')
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    }
+};
